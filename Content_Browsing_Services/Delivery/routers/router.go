2af92c1d80c71feeafe75448088c8ffbf611484f
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-09-08 11:04:34 +0300

    Update router.go

--- Content_Browsing_Services/Delivery/routers/router.go
+++ Content_Browsing_Services/Delivery/routers/router.go
@@ -23,7 +23,7 @@ func NewRouter(
 	config := cors.Config{
         AllowOrigins: []string{
             "http://localhost:3000",
-			"https://lawgen.vercel.app",
+			"https://lawgen-frontend-wine.vercel.app",
         },
         AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "X-Client-Type"},
